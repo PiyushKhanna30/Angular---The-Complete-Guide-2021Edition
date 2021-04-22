b845aaa4d5fae5e0b6ccd82ae3246f0a6b215359
Piyush Khanna <khanna.piyush30@gmail.com>
2021-04-23 01:57:24 +0530

    back to normal css

--- Section 5/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ Section 5/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,10 +1,9 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css'],
-  encapsulation:ViewEncapsulation.None
+  styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
   @Input('serverElement') element: { type: string, name: string, content: string };
