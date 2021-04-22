b5a0f1a4d0cc03aa554d3187501d0a6e4a10b09d
Piyush Khanna <khanna.piyush30@gmail.com>
2021-04-23 02:47:03 +0530

    access dom with @ViewChild

--- Section 5/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ Section 5/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,22 +8,23 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName:string, serverContent:string}>();
   @Output("bpCreated") bluePrintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
-  newServerContent = '';
+
+  @ViewChild('serverContentInput', {static: true}) serverContentInput:ElementRef;
   constructor() { }
 
   ngOnInit(): void {
   }
   onAddServer(serverNameInput:HTMLInputElement) {
-    console.log(serverNameInput.value);
+    console.log(this.serverContentInput);
     this.serverCreated.emit({
       serverName:serverNameInput.value,
-      serverContent:this.newServerContent
+      serverContent:this.serverContentInput.nativeElement.value
     });
   }
   onAddBlueprint(serverNameInput:HTMLInputElement) {
     this.bluePrintCreated.emit({
       serverName:serverNameInput.value,
-      serverContent:this.newServerContent
+      serverContent:this.serverContentInput.nativeElement.value
     });
   }
 }
