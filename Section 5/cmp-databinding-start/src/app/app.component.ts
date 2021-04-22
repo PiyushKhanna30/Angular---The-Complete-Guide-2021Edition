38280ca58edb31805d998b1a57e027ae596b92e6
Piyush Khanna <khanna.piyush30@gmail.com>
2021-04-23 01:14:16 +0530

    @Output demo to send data from child to parent component

--- Section 5/cmp-databinding-start/src/app/app.component.ts
+++ Section 5/cmp-databinding-start/src/app/app.component.ts
@@ -6,8 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type:'server', name:'Server 1', content:'Hello World!'}];
-  newServerName = '';
-  newServerContent = '';
-
+  serverElements = [{ type: 'server', name: 'Server 1', content: 'Hello World!' }];
+  onServerAdded(serverData:{serverName:string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+  onBlueprintAdded(blueprintData:{serverName:string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
