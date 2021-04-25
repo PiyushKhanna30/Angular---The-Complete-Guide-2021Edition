379c9c36d7a0258f3b97fd12c8eaee92e26c37d1
Piyush Khanna <khanna.piyush30@gmail.com>
2021-04-25 20:42:43 +0530

    Injecting service to another

--- Section 9/services-start/src/app/account/account.component.ts
+++ Section 9/services-start/src/app/account/account.component.ts
@@ -1,19 +1,16 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountService } from '../account.service';
-import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  styleUrls: ['./account.component.css']
 })
 export class AccountComponent {
   @Input() account: { name: string, status: string };
   @Input() id: number;
-  constructor(private loggingService: LoggingService, private accountService: AccountService) { }
+  constructor(private accountService: AccountService) { }
   onSetTo(status: string) {
     this.accountService.updateStatus(this.id, status);
-    this.loggingService.logStatusChange(status);
   }
 }
--- Section 9/services-start/src/app/new-account/new-account.component.ts
+++ Section 9/services-start/src/app/new-account/new-account.component.ts
@@ -1,18 +1,14 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { AccountService } from '../account.service';
-import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
-  constructor(private loggingService: LoggingService, 
-              private accountService: AccountService) { }
+  constructor(private accountService: AccountService) { }
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
   }
 }
