185e341f0a7d1a9abfefe1880f641eac3c84eb3c
Piyush Khanna <khanna.piyush30@gmail.com>
2021-04-26 02:23:06 +0530

    page not found routing

--- Section 11/routing-start/src/app/servers/server/server.component.ts
+++ Section 11/routing-start/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -11,16 +11,23 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: { id: number, name: string, status: string };
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute, private router:Router) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
-    console.log("Id : "+id);
+    console.log("Id : " + id);
     this.route.params.subscribe(
       (params: Params) => {
         this.server = this.serversService.getServer(+params['id']);
       }
     );
+  }
+  onEdit() {
+    const id:number = +this.route.snapshot.queryParams['allowEdit'];
+    console.log(id);
+    if(id==1){
+      this.router.navigate(['/servers',id, 'edit']);
+    }
   }
 
 }
