07fae94ea13d52c708d62de52804c1f8812ec594
Piyush Khanna <khanna.piyush30@gmail.com>
2021-05-11 01:24:07 +0530

    TD handling radio buttons

--- Section 15/forms-td-start/src/app/app.component.ts
+++ Section 15/forms-td-start/src/app/app.component.ts
@@ -8,7 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   defaultOption = "pet";
-  answere = ''
+  answere = '';
+  genders = ['male','female'];
   @ViewChild('myForm', {static:true})myForm:NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
