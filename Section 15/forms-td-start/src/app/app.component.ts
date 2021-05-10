e070cdda78298fc8903b2be546b270239c732d9c
Piyush Khanna <khanna.piyush30@gmail.com>
2021-05-11 01:01:52 +0530

    TD property binding using ngModel

--- Section 15/forms-td-start/src/app/app.component.ts
+++ Section 15/forms-td-start/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  defaultOption = "pet";
   @ViewChild('myForm', {static:true})myForm:NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
