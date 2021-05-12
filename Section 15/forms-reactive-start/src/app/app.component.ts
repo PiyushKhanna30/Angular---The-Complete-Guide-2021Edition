9e05e02376aa29b97221283aac3c3c187cff0f8f
Piyush Khanna <khanna.piyush30@gmail.com>
2021-05-12 13:51:59 +0530

    RT grouping controls

--- Section 15/forms-reactive-start/src/app/app.component.ts
+++ Section 15/forms-reactive-start/src/app/app.component.ts
@@ -6,17 +6,19 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
-  ngOnInit(){
+  ngOnInit() {
     this.signupForm = new FormGroup({
-      'username':new FormControl(null, Validators.required),
-      'email':new FormControl(null, [Validators.required, Validators.email]),
-      'gender':new FormControl('male')
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      'gender': new FormControl('male')
     })
   }
-  onSubmit(){
+  onSubmit() {
     console.log(this.signupForm);
   }
 }
