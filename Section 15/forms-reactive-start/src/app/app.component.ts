51ee5166a0400cb04a87999d857d122e4f874aaf
Piyush Khanna <khanna.piyush30@gmail.com>
2021-05-12 17:06:40 +0530

    RT custom validation

--- Section 15/forms-reactive-start/src/app/app.component.ts
+++ Section 15/forms-reactive-start/src/app/app.component.ts
@@ -8,11 +8,12 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  forbiddenUsernames = ['chris','anna'];
   signupForm: FormGroup;
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -26,5 +27,12 @@ export class AppComponent implements OnInit {
   addHobby(){
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  forbiddenNames(control:FormControl):{[s:string]:boolean}{
+    if(this.forbiddenUsernames.indexOf(control.value)!==-1){
+      return {'nameIsForbidden':true};
+    }
+    return null;
   }
 }
