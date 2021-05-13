9c19c77ab10e7af6b13132d670cae86455baeeab
Piyush Khanna <khanna.piyush30@gmail.com>
2021-05-13 19:00:36 +0530

    transforming response data using pipe and map

--- Section 18/http-01-start/src/app/app.component.ts
+++ Section 18/http-01-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -29,9 +30,17 @@ export class AppComponent implements OnInit {
     // Send Http request
   }
 
-  private fetchPosts(){
-    this.http.get('https://ng-complete-guide-43c71-default-rtdb.firebaseio.com/posts.json').subscribe(posts => {
-      console.log(posts);
-    });
+  private fetchPosts() {
+    this.http.get('https://ng-complete-guide-43c71-default-rtdb.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
+        const postsArray = [];
+        for (const key in responseData) {
+          postsArray.push({ ...responseData[key], id: key });
+        }
+        return postsArray;
+      }))
+      .subscribe(posts => {
+        console.log(posts);
+      });
   }
 }
